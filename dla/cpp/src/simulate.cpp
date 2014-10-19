dd47a491c7d4bdbb1e3998ec75644c61cd87bdc6
Paul Tan <pjxtan@gmail.com>
2014-10-19 18:48:24 -0400

    Get grid width, height and output filename from arguments

--- dla/cpp/src/simulate.cpp
+++ dla/cpp/src/simulate.cpp
@@ -3,8 +3,12 @@
 #include "dla.hpp"
 
 int main(int argc, char *argv[]) {
-    Dla dla = Dla(501, 501);
+    int grid_width = std::atoi(argv[1]);
+    int grid_height = std::atoi(argv[2]);
+    char *outfile = (argv[3]);
+    Dla dla = Dla(grid_width, grid_height);
     dla.sim();
-    dla.print_grid();
+    //dla.print_grid();
+    dla.write_grid_to_file(outfile);
     return 0;
 }
