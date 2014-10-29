31dda077235f334705417119b3d799da9fdac976
Paul Tan <pjxtan@gmail.com>
2014-10-28 20:47:30 -0400

    Draw lattice in real-time

--- dimer_covering/java/Main.java
+++ dimer_covering/java/Main.java
@@ -1,34 +1,49 @@
 import java.util.Random;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
         // Get these from user input later
-        double tempFinal = 1e-3;
-        double tempInit = 10.0;
-        double tau = 1e4;
+        final int width = 50;
+        final int height = 50;
+        final double tempFinal = 1e-3;
+        final double tempInit = 10.0;
+        final double tau = 1e4;
+        final int frameSkip = 0;
+
         Random random = new Random();
 
-        Grid grid = new Grid(50, 50);
         int t = 0;
         double temp = tempInit;
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width * 10 + 50, height * 10 + 50);
+        frame.setVisible(true);
+
+        GridPanel gridPanel = new GridPanel(width, height);
+        frame.add(gridPanel);
 
         while (temp > tempFinal) {
             t++;
             temp = cooling(t, tau, tempInit);
 
             // Do the Markov chain
-            Coordinate[] sites = grid.pickSites();
-            if (grid.hasSingleDimer(sites)) {
+            Coordinate[] sites = gridPanel.grid.pickSites();
+            if (gridPanel.grid.hasSingleDimer(sites)) {
                 if (random.nextDouble() < Math.exp(-1 / temp))
-                        grid.removeDimer(sites);
+                        gridPanel.grid.removeDimer(sites);
             }
-            else if (grid.cells.get(sites[0]).dimer == null
-                     && grid.cells.get(sites[1]).dimer == null)
-                grid.placeDimer(sites);
+            else if (gridPanel.grid.cells.get(sites[0]).dimer == null
+                     && gridPanel.grid.cells.get(sites[1]).dimer == null)
+                gridPanel.grid.placeDimer(sites);
             
-            if (t % 10000 == 0)
-                System.out.format("%d %f %d\n", t, temp, grid.nDimers);
+            if (t % (frameSkip + 1) == 0) {
+                System.out.format("%d %f %d\n", t, temp, gridPanel.grid.nDimers);
+                gridPanel.repaint();
+            }
         }
+
     }
 
     /**
